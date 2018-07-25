d1ec5ec36738f663d75261b507d918b918a40094
Anton Lapshin <anton.lapshin@sixt.com>
2018-07-25 23:19:12 +0200

    Dashboards styles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,31 +1,51 @@
-import React from "react";
-import { items } from "./meta";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./Dashboard.css";
-import Editor from "./Editor";
+import React from 'react';
+import { items } from './meta';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { debounce } from 'lodash';
+import './Dashboard.css';
+import Editor from './Editor';
 
-const List = () => {
-  const listItems = items.map((c, i) => {
-    return (
-      <li key={i}>
-        <Link to={`/editor/${c.name}`}>{c.name}</Link>
-      </li>
-    );
-  });
-  return (
-    <div>
-      <ul>{listItems}</ul>
-    </div>
-  );
-};
+class List extends React.Component {
+    state = { pattern: '' };
+
+    updatePattern = debounce(value => {
+        this.setState({ pattern: value });
+    }, 100);
+
+    render() {
+        const pattern = this.state.pattern.toLowerCase();
+        const listItems = items
+            .filter(item => item.name.toLowerCase().includes(pattern))
+            .map((item, i) => {
+                return (
+                    <li key={i}>
+                        <Link to={`/editor/${item.name}`}>{item.name}</Link>
+                    </li>
+                );
+            });
+
+        return (
+            <div className="dashboard">
+                <input
+                    className="pattern"
+                    type="text"
+                    value={pattern}
+                    placeholder="Enter your component name"
+                    onChange={e => this.updatePattern(e.target.value)}
+                />
+                <ul className="items">{listItems}</ul>
+            </div>
+        );
+    }
+}
 
 const Dashboard = () => (
-  <Router>
-    <React.Fragment>
-      <Route exact path="/" component={List} />
-      <Route path="/editor/:name" component={Editor} />
-    </React.Fragment>
-  </Router>
+    <Router>
+        <React.Fragment>
+            <Route exact path="/" component={List} />
+            <Route path="/editor/:name" component={Editor} />
+        </React.Fragment>
+    </Router>
 );
 
 export default Dashboard;
